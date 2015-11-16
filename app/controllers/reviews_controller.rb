66f72b48a1bc5b49c713ea5baf3a91f161a57a15
Thomas Solow <thomassolow@gmail.com>
2015-11-16 10:12:07 -0500

    added delete, edit, and user auth for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
   def index
     @reviews = Review.all
   end
@@ -9,7 +10,7 @@ class ReviewsController < ApplicationController
     # show method will access the review and its associated
     # subject
     @review = Review.find(params[:id])
-    if @review.category.downcase == "film" || "movie" || "show" || "tv show" || "series"
+    if @review.category.downcase == "movie" || "show"
       @movie_info = HTTParty.get("http://www.omdbapi.com/?t=#{@review.name}&plot=short&r=json")
     elsif @review.category.downcase == "album"
       RSpotify.raw_response = true
@@ -18,13 +19,33 @@ class ReviewsController < ApplicationController
 
   end
   def new
+    @review = Review.new
   end
   def create
+    @review = current_user.reviews.new(review_params)
+      if @review.save
+        flash[:notice] = "Review was successfully created."
+        redirect_to @review
+      else
+        flash[:alert] = "Review creation failed, likely missing a needed field."
+        render :new
+      end
   end
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
   end
   def edit
+    @review = Review.find(params[:id])
   end
   def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to review_path(@review)
+  end
+  private
+  def review_params
+    params.require(:review).permit(:text, :title, :score, :name, :category)
   end
 end
